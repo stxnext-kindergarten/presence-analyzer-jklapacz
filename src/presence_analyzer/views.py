656eee3e54221571d4f693b6b36a10a988303dac
jacek klapacz <jacek.klapacz@stxnext.pl>
2015-09-08 10:35:55 +0200

    [CC-6205] add timeline screen

--- src/presence_analyzer/views.py
+++ src/presence_analyzer/views.py
@@ -7,8 +7,7 @@ import calendar
 from flask import redirect, abort
 
 from presence_analyzer.main import app
-from presence_analyzer.utils import jsonify, get_data, mean, \
-    group_by_weekday, group_start_end_by_weekday
+from presence_analyzer import utils
 
 import logging
 log = logging.getLogger(__name__)  # pylint: disable=invalid-name
@@ -23,12 +22,12 @@ def mainpage():
 
 
 @app.route('/api/v1/users', methods=['GET'])
-@jsonify
+@utils.jsonify
 def users_view():
     """
     Users listing for dropdown.
     """
-    data = get_data()
+    data = utils.get_data()
     return [
         {'user_id': i, 'name': 'User {0}'.format(str(i))}
         for i in data.keys()
@@ -36,19 +35,19 @@ def users_view():
 
 
 @app.route('/api/v1/mean_time_weekday/<int:user_id>', methods=['GET'])
-@jsonify
+@utils.jsonify
 def mean_time_weekday_view(user_id):
     """
     Returns mean presence time of given user grouped by weekday.
     """
-    data = get_data()
+    data = utils.get_data()
     if user_id not in data:
         log.debug('User %s not found!', user_id)
         abort(404)
 
-    weekdays = group_by_weekday(data[user_id])
+    weekdays = utils.group_by_weekday(data[user_id])
     result = [
-        (calendar.day_abbr[weekday], mean(intervals))
+        (calendar.day_abbr[weekday], utils.mean(intervals))
         for weekday, intervals in enumerate(weekdays)
     ]
 
@@ -56,17 +55,17 @@ def mean_time_weekday_view(user_id):
 
 
 @app.route('/api/v1/presence_weekday/<int:user_id>', methods=['GET'])
-@jsonify
+@utils.jsonify
 def presence_weekday_view(user_id):
     """
     Returns total presence time of given user grouped by weekday.
     """
-    data = get_data()
+    data = utils.get_data()
     if user_id not in data:
         log.debug('User %s not found!', user_id)
         abort(404)
 
-    weekdays = group_by_weekday(data[user_id])
+    weekdays = utils.group_by_weekday(data[user_id])
     result = [
         (calendar.day_abbr[weekday], sum(intervals))
         for weekday, intervals in enumerate(weekdays)
@@ -77,19 +76,19 @@ def presence_weekday_view(user_id):
 
 
 @app.route('/api/v1/presence_start_end/<int:user_id>', methods=['GET'])
-@jsonify
+@utils.jsonify
 def presence_start_end_view(user_id):
     """
     Returns mean start & end time of given user grouped by weekday
     (mean time when user begins work, mean time when user finish work)
     """
-    data = get_data()
+    data = utils.get_data()
     if user_id not in data:
         log.debug('User %s not found!', user_id)
         abort(404)
 
-    weekdays = group_start_end_by_weekday(data[user_id])
+    weekdays = utils.group_start_end_by_weekday(data[user_id])
     return [
-        (calendar.day_abbr[weekday], mean(items['start']), mean(items['end']))
+        (calendar.day_abbr[weekday], utils.mean(items['start']), utils.mean(items['end']))
         for weekday, items in enumerate(weekdays)
     ]
